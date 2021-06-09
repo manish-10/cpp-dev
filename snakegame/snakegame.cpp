2dea3b1f9937f114fc9e35ea61f420979f87cc46
manish-10 <mks19121999@gmail.com>
2021-06-09 19:37:57 +0530

    Added logic for the game
    Signed-off-by: manish-10 <mks19121999@gmail.com>

--- snakegame/snakegame.cpp
+++ snakegame/snakegame.cpp
@@ -76,7 +76,7 @@ void Input()
 			dir = LEFT;
 			break;
 		case 'd':
-			dir = RIGHT;
+			dir = RIGHT;    
 			break;
 		case 'w':
 			dir = UP;
@@ -91,8 +91,66 @@ void Input()
 	}
 }
 
+void Logic()
+{
+	int prevX = tailX[0];
+	int prevY = tailY[0];
+	int prev2X, prev2Y;
+	tailX[0] = x;
+	tailY[0] = y;
+	for (int i = 1; i < nTail; i++)
+	{
+		prev2X = tailX[i];
+		prev2Y = tailY[i];
+		tailX[i] = prevX;
+		tailY[i] = prevY;
+		prevX = prev2X;
+		prevY = prev2Y;
+	}
+	switch (dir)
+	{
+	case LEFT:
+		x--;
+		break;
+	case RIGHT:
+		x++;
+		break;
+	case UP:
+		y--;
+		break;
+	case DOWN:
+		y++;
+		break;
+	default:
+		break;
+	}
+	//if (x > width || x < 0 || y > height || y < 0)
+	//	gameOver = true;
+	if (x >= width) x = 0; else if (x < 0) x = width - 1;
+	if (y >= height) y = 0; else if (y < 0) y = height - 1;
+
+	for (int i = 0; i < nTail; i++)
+		if (tailX[i] == x && tailY[i] == y)
+			gameOver = true;
+
+	if (x == fruitX && y == fruitY)
+	{
+		score += 10;
+		fruitX = rand() % width;
+		fruitY = rand() % height;
+		nTail++;
+	}
+}
+
 int main()
 {
-	Draw();
+	Setup();
+	while (!gameOver)
+	{
+		Draw();
+		Input();
+		Logic();
+		Sleep(10); //sleep(10);
+	}
 	return 0;
 }
