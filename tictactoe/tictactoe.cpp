40c024b8a4269238d55f1080984139b483c97d84
manish-10 <mks19121999@gmail.com>
2021-06-09 18:53:23 +0530

    Added draw function
    Signed-off-by: manish-10 <mks19121999@gmail.com>

--- tictactoe/tictactoe.cpp
+++ tictactoe/tictactoe.cpp
@@ -4,7 +4,16 @@ char matrix[3][3] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 char player = 'X';
 void Draw()
 {
-	
+	system("cls");
+	cout << "Tic Tac Toe v1.0" << endl;
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << matrix[i][j] << " ";
+		}
+		cout << endl;
+	}
 }
 void Input()
 {
