61b74f5fbd995a078ae824817bd2397de51a837d
manish-10 <mks19121999@gmail.com>
2021-06-09 18:52:11 +0530

    initial fuction
    Signed-off-by: manish-10 <mks19121999@gmail.com>

---
+++ tictactoe/tictactoe.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+char matrix[3][3] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+char player = 'X';
+void Draw()
+{
+	
+}
+void Input()
+{
+	
+}
+void TogglePlayer()
+{
+	
+}
+char Win()
+{
+	
+}
+int main()
+{
+	Draw();
+	
+	return 0;
+}
